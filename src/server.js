0b614b42f7b109df4453dee18bfdac660444ec8d
Melony Smith <smith.melonya@gmail.com>
2017-01-12 18:01:26 -0600

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -3,8 +3,16 @@
 // dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const chalk = require('chalk');
+const utility = require('./lib/debug');
+const log = require('./lib/log');
 
+// require dotenv
 require("dotenv").config();
+
+// chalk rules
+const active = chalk.white;
+const envVar = chalk.gray;
 
 // instantiate express
 const app = express();
@@ -21,9 +29,12 @@ app.use(bodyParser.json());
 // express uses routes directory
 app.use('/', require('./routes')(express));
 
+// environmental variable set to
+  log(envVar('Environmental variable is defined as ' + process.env.DEBUG));
+
 // set up server
 const server = app.listen(port, () => {
-  console.log('STILL on like Donkey Kong on Port ' + port);
+  utility.debug(active('STILL on like Donkey Kong on Port ' + port));
 });
 
 // export server
